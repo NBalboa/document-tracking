c26cbe0525a9aff81c56d32db581cc30765e39c2
NBalboa <nickojek2x@gmail.com>
2025-12-29 23:08:04 +0800

    added show and hide tracks on reports

--- resources/js/pages/User/Reports/Reports.tsx
+++ resources/js/pages/User/Reports/Reports.tsx
@@ -11,9 +11,7 @@ import { router } from '@inertiajs/react'
 import ReportController from '@/actions/App/Http/Controllers/ReportController'
 import { Track } from '../Track/types'
 import { Department, User } from '../Users/types'
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
-import { formatDate } from '@/lib/formatDate'
-import { Badge } from '@/components/ui/badge'
+import Report from './parts/Report'
 
 
 const Reports = ({
@@ -165,52 +163,7 @@ const Reports = ({
                 </div>
                 <div className='space-y-2'>
                     {documents.map((document) => (
-                        <Card key={document.id}>
-                            <CardHeader>
-                                <div className='flex justify-between items-center'>
-                                    <div className='space-y-1'>
-                                        <p className='text-muted-foreground'>ID: {document.id}</p>
-                                        <CardTitle>{document.name} -
-                                            <Badge className='ms-2 capitalize'>{document.latest_track.status}</Badge>
-                                        </CardTitle>
-                                    </div>
-                                    <CardTitle>{formatDate({ date: document.created_at })}</CardTitle>
-                                </div>
-                                <CardDescription>Author: {document.user.first_name} {document.user.last_name}</CardDescription>
-                                <CardDescription>Deparment: {document.user.department.name}</CardDescription>
-                                <CardDescription>Type: {document.document_type.name}</CardDescription>
-                                <CardDescription>Description: {document.description}</CardDescription>
-                                <CardDescription>Others: {document.others}</CardDescription>
-                            </CardHeader>
-                            <CardContent>
-                                <div>
-                                    {document.tracks.map((track) => (
-                                        <div className="space-y-8" key={track.id}>
-                                            <div className="flex relative">
-                                                <div className="space-y-1 text-right w-48 pe-5">
-                                                    <h3 className="font-medium text-sm">{track.created_at.date}</h3>
-                                                    <span className="text-sm text-gray-500">{track.created_at.time}</span>
-                                                </div>
-                                                <div className="relative">
-                                                    <div className="absolute left-1/2 -translate-x-1/2 top-0 bottom-0 border-l border-gray-300"></div>
-                                                    <div
-                                                        className="w-3 h-3 bg-background border-2 border-primary rounded-full absolute left-1/2 top-0 -translate-x-1/2 z-10">
-                                                    </div>
-                                                </div>
-                                                <div className="space-y-1 ps-5 pb-5">
-                                                    <h4 className="font-semibold text-text-foreground capitalize text-sm">{track.status}</h4>
-                                                    <p className="text-foreground text-sm">{track.user.first_name} {track.user.last_name}</p>
-                                                    <h2 className="text-muted-foreground text-sm">{track.user.department.name}</h2>
-                                                    {track.remarks && (
-                                                        <p className='text-muted-foreground text-sm'>{track.remarks}</p>
-                                                    )}
-                                                </div>
-                                            </div>
-                                        </div>
-                                    ))}
-                                </div>
-                            </CardContent>
-                        </Card>
+                        <Report document={document} key={document.id} />
                     ))}
                 </div>
             </div>
